1760644bcca3bd4c0593e7ef4ae490541c21dd4c
Japnoor Singh <109523523+Japnoor2104@users.noreply.github.com>
2023-06-09 18:50:32 +0530

    change

--- app/src/main/java/com/japnoor/chatify/HomeScreen.kt
+++ app/src/main/java/com/japnoor/chatify/HomeScreen.kt
@@ -3,19 +3,44 @@ package com.japnoor.chatify
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
 import com.japnoor.chatify.databinding.ActivityHomeScreenBinding
 
 
 class HomeScreen : AppCompatActivity() {
-    lateinit var binding : ActivityHomeScreenBinding
+    lateinit var binding: ActivityHomeScreenBinding
+    lateinit var usersList: ArrayList<Any>
+    lateinit var userRef: DatabaseReference
+    lateinit var homeScreenAdapter: HomeScreenAdapter
+    lateinit var auth : FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityHomeScreenBinding.inflate(layoutInflater)
+        binding = ActivityHomeScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        auth=FirebaseAuth.getInstance()
+        userRef = FirebaseDatabase.getInstance().reference.child("chat")
+        homeScreenAdapter = HomeScreenAdapter()
+        usersList = ArrayList()
 
         binding.allChatActivityBtn.setOnClickListener {
-            var intent= Intent(this@HomeScreen,AllChatsActivity::class.java)
+            var intent = Intent(this@HomeScreen, AllChatsActivity::class.java)
             startActivity(intent)
         }
+
+        userRef.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                for(each in snapshot.children){
+                    var users=each.getValue(Any::class.java)
+                    println(each.getValue(Any::class.java))
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
     }
 }
---
+++ app/src/main/java/com/japnoor/chatify/HomeScreenAdapter.kt
@@ -0,0 +1,11 @@
+package com.japnoor.chatify
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeScreenAdapter : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_screen_adapter)
+    }
+}
